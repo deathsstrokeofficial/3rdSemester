b30969c684465e375daaa59b77003708f23c2a5d
deathsstrokeofficial <deathsstrokeofficial@gmail.com>
2021-10-07 01:31:08 +0530

    Add notes 06.10.21

--- DSA Lab/L2_Stack.c
+++ DSA Lab/L2_Stack.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void push();
+int * push(int *, int *);
+int * pop(int *, int *);
+void display(int *, int *);
 
 int main()
 {
@@ -14,18 +16,73 @@ int main()
     while(1)
     {
         printf("\n 1. PUSH");
-        printf("\n 2. EXIT\n");
+        printf("\n 2. POP");
+        printf("\n 3. EXIT\n");
         scanf("%d", &n);
 
         if (n==1)
-            push();
+            top = push(ar, top);
+        else if (n==2)
+            top = pop(ar, top);
         else
             exit(0);
     }
     return 0;
 }
 
-void push()
+int * push(int *ar1, int *top)
+{
+    int i, j;
+
+    printf("\nEnter the number you want to push: ");
+    scanf("%d", &i);
+
+    if (top == NULL)
+    {
+        top = &ar1[0];
+        *top = i;
+    }
+    else
+    {
+        if (top == &ar1[9])
+        {
+            printf("\nOVERFLOW");
+        }
+        else
+        {
+            top++;
+            *top = i;
+        }
+    }
+    display(ar1, top);
+    return top;
+}
+
+void display(int *ar2, int *top)
 {
 
+    int *i;
+    for (i = top; i >= ar2; i--)
+    {
+        printf("\n%d", *i);
+    }
+}
+
+int * pop(int *ar3, int *top)
+{
+    if (top == NULL)
+    {
+        printf("Underflow");
+        return;
+    }
+    if (*top == ar3[0])
+    {
+        top = NULL;
+        display(ar3, top);
+        return top;
+    }
+
+    top--;
+    display(ar3, top);
+    return top;
 }
